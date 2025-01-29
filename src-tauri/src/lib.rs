1261fbbc42d9db5038b690381c0ab3a6311e8998
YofaGh <ghadiriyosef123@gmail.com>
2025-01-30 01:06:48 +0330

    orginized dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,7 +11,6 @@ use utils::load_up_checks;
 pub fn run() {
     let state: Arc<Mutex<AppState>> = Arc::new(Mutex::new(AppState { cancel_tx: None }));
     Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
         .manage(state.clone())
         .invoke_handler(generate_handler![get_data, cancel_request])
